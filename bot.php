a5b77850b75384d68f99277aa708deedb0b7e41b
praphatpong ngamchuen <phblepyu@hotmail.com>
2018-02-15 10:27:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'Yz5rtFrX/Tg8iQmp6fIcNsRzZyPnp+JHGPeo/6CkdNzHweNvX0uDHpAoqDqFIDWqvv0AiMgCwI6kFzDU6/moas/tF2AGaT7P7firyzz1AXVMTKeWAUHhEesZ2lLpqQCqzL8WO56Ut7w6I7fpFR3hKgdB04t89/1O/w1cDnyilFU=';
+$access_token = 'omUZsqxWgzt/wC8KFJONbC6hOrz4i9mgqAfP+Tkrw37mv/Y9E+lZBHFo60pYFhBdvv0AiMgCwI6kFzDU6/moas/tF2AGaT7P7firyzz1AXXgJzRVUMXHFZizLUq1VeeV5cFYaNq6Ush23Jmdgtz7owdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
