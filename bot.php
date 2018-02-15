32f62fe625dbd39a0e7cf669eb8b44c11bd80f2b
praphatpong ngamchuen <phblepyu@hotmail.com>
2018-02-15 14:28:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,8 +26,8 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $rig = file_get_contents('https://api.nanopool.org/v1/eth/user/:0x817fb2a01d6d115a84f3abce3261d53294c30517');
-  $arrPostData['messages'][0]['text'] = $rig;
+  $rig = file_get_contents('https://api.nanopool.org/v1/eth/user/0x817fb2a01d6d115a84f3abce3261d53294c30517');
+  $arrPostData['messages'][0]['text'] = $rig['data']['balance'];
 }
 else{
   $arrPostData = array();
