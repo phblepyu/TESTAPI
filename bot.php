61a9ddde2b5e5036362dcb2bd373647993517a51
praphatpong ngamchuen <phblepyu@hotmail.com>
2018-02-19 11:59:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -45,7 +45,12 @@ $result = file_get_contents($url, false, $context);
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
+ if ($result != ""){
   $arrPostData['messages'][0]['text'] = $result; 
+ }else{
+  $arrPostData['messages'][0]['text'] = "หนูยังไม่สามารถตอบคำถามนี้ได้ค่ะ";
+ }
+ 
 }
  
  
