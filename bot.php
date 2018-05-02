4cb63ea9a233d16b097042cd5962826d8d006f33
praphatpong ngamchuen <phblepyu@hotmail.com>
2018-05-02 09:06:00 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,7 +23,7 @@ $arrHeader[] = "Authorization: Bearer {jITXrS8kODVqVYpvleRFusP8rUvJ4zQjd65ARZ0fs
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = file_get_contents('http://49.231.234.79/linebot/ethble.php'); 
 }
-else if($arrJson['events'][0]['message']['text'] == "ปฎิทิน"){
+else if($arrJson['events'][0]['message']['text'] == "ปฏิทิน"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
