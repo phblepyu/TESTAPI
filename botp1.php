d9b28d1c48ee88ae9068118348f93f028206cc2e
praphatpong ngamchuen <phblepyu@hotmail.com>
2018-06-11 15:10:43 +0700

    Update botp1.php

--- botp1.php
+++ botp1.php
@@ -76,6 +76,68 @@ switch ($arrJson['events'][0]['message']['text']) {
    $arrPostData['messages'][0]['latitude'] = 18.725752;
    $arrPostData['messages'][0]['longitude'] = 98.958495;
     break;
+  
+  case 'ไอ':
+   $arrPostData = array();
+   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+   $arrPostData['messages'][0] = array(
+                'type' => 'template', // 訊息類型 (模板)
+                'altText' => 'Example buttons template', // 替代文字
+                'template' => array(
+                    'type' => 'carousel', // 類型 (旋轉木馬)
+                    'columns' => array(
+                        array(
+                            'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
+                            'title' => 'Example Menu 1', // 標題 1 <不一定需要>
+                            'text' => 'Description 1', // 文字 1
+                            'actions' => array(
+                                array(
+                                    'type' => 'postback', // 類型 (回傳)
+                                    'label' => 'postback 1', // 標籤 1
+                                    'data' => 'action=buy&itemid=123' // 資料
+                                ),
+                                array(
+                                    'type' => 'message', // 類型 (訊息)
+                                    'label' => 'Message example 1', // 標籤 2
+                                    'text' => 'Message example 1' // 用戶發送文字
+                                ),
+                                array(
+                                    'type' => 'uri', // 類型 (連結)
+                                    'label' => 'Uri example 1', // 標籤 3
+                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+                                )
+                            )
+                        ),
+                        array(
+                            'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
+                            'title' => 'Example Menu 2', // 標題 2 <不一定需要>
+                            'text' => 'Description 2', // 文字 2
+                            'actions' => array(
+                                array(
+                                    'type' => 'postback', // 類型 (回傳)
+                                    'label' => 'postback 2', // 標籤 1
+                                    'data' => 'action=buy&itemid=123' // 資料
+                                ),
+                                array(
+                                    'type' => 'message', // 類型 (訊息)
+                                    'label' => 'Message example 2', // 標籤 2
+                                    'text' => 'Message example 2' // 用戶發送文字
+                                ),
+                                array(
+                                    'type' => 'uri', // 類型 (連結)
+                                    'label' => 'Uri example 2', // 標籤 3
+                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+            );
+   //$arrPostData['messages'][0]['packageId'] = '1';
+   //$arrPostData['messages'][0]['stickerId'] = '100';
+    break;
   default:
     // code...
     break;
